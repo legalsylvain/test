cd88e34c03d552760a0391692ffdb27c80eb3c26
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-06-14 23:27:54 +0200

    [TEST]

--- check_flake8.py
+++ check_flake8.py
@@ -1,14 +1,24 @@
 import config
 from flake8 import main as flake8_lib
-from subprocess import call
+from subprocess import check_call, CalledProcessError
+
 
 def _check_folder(path):
-        call(["flake8", path, "--filename=__init__.py", "--ignore=F401"])
-        call(["flake8", path,"--exclude=__init__.py", "--ignore=F841"])
+    try:
+        check_call(["flake8", path, "--filename=__init__.py", "--ignore=F401"])
+        chekc_call(["flake8", path,"--exclude=__init__.py", "--ignore=F841"])
+        return True
+    except CalledProcessError:
+        return False
+
+ok = True
 
 for rep in config.REPOSITORIES:
     if rep['modules']:
         for mod in rep['modules']:
-            _check_folder('./%s/%s' % (rep['name'], mod))
+            ok = _check_folder('./%s/%s' % (rep['name'], mod)) and ok
     else:
-        _check_folder('./%s' % (rep['name']))
+        ok = _check_folder('./%s' % (rep['name'])) and ok
+
+if not ok:
+    raise Exception('Flake8 Error')
