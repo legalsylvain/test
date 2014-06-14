9732b1f466e3b47571f1cdb43e26554e9e7ec585
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-06-14 23:40:35 +0200

    [TEST]

--- check_flake8.py
+++ check_flake8.py
@@ -8,7 +8,7 @@ print os.listdir('.')
 def _check_folder(path):
     try:
         check_call(["flake8", path, "--filename=__init__.py", "--ignore=F401"])
-        chekc_call(["flake8", path,"--exclude=__init__.py", "--ignore=F841"])
+        check_call(["flake8", path,"--exclude=__init__.py", "--ignore=F841"])
         return True
     except CalledProcessError:
         return False
