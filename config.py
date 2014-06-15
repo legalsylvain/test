6e4559f6cdecab1539e6686283434aacfdea3441
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-06-15 02:56:48 +0200

    [FIX]

--- config.py
+++ config.py
@@ -27,21 +27,21 @@ COMPLETE_TEST = True
 DATABASE_NAME = 'test__all'
 
 REPOSITORIES = [
-#    {
-#        'name': 'server-env-tools',
-#        'modules': [
-#            'auth_admin_passkey', 'disable_openerp_online']},
-#    {
-#        'name': 'web-addons',
-#        'modules': [
-#            'web_ckeditor4', 'web_confirm_window_close', 'web_export_view',
-#            'web_popup_large', 'web_widget_float_formula']},
-#    {
-#        'name': 'openerp-addons-cis',
-#        'modules': None},
-#    {
-#        'name': 'openerp-addons-grap',
-#        'modules': None},
+    {
+        'name': 'server-env-tools',
+        'modules': [
+            'auth_admin_passkey', 'disable_openerp_online']},
+    {
+        'name': 'web-addons',
+        'modules': [
+            'web_ckeditor4', 'web_confirm_window_close', 'web_export_view',
+            'web_popup_large', 'web_widget_float_formula']},
+    {
+        'name': 'openerp-addons-cis',
+        'modules': None},
+    {
+        'name': 'openerp-addons-grap',
+        'modules': None},
 ]
 
 ADDONS_LIST = './' + ',./'.join(
