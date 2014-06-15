a3041ebf7330ed07938e6d4a49e281a47b255890
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-06-15 11:54:24 +0200

    [TEST]

--- check_test_all.py
+++ check_test_all.py
@@ -1,7 +1,7 @@
 import config
 from subprocess import check_call
 
-for module in config.MODULES_LIST:
+for module in config.MODULES:
     check_call([
         'coverage', 'run', './openerp-command/oe', 'run-tests',
         '--database=%s' % (config.DATABASE_NAME),
